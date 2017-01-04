1d221f305f2cc90445452917a217275f391e70cd
David Owen Walker <walk.dv@gmail.com>
2017-01-04 15:48:10 -0400

    add startLocation to tour API

--- models/Api/Tour.php
+++ models/Api/Tour.php
@@ -13,14 +13,21 @@ class Api_Tour extends Omeka_Record_Api_AbstractRecordAdapter
     {
 
         $db = get_db();
+        
         $tiTable = $db->getTable( 'TourItem' );
+        $glTable = $db->getTable( 'Geolocation' );
 
-        $select = $tiTable->getSelect();
-        $select->where( 'tour_id = ?', array( $record->id ) );
+        $tiSelect = $tiTable->getSelect();
+        $tiSelect->where( 'tour_id = ?', array( $record->id ) );
 
         # Get the tour items
-        $tourItems = $tiTable->fetchObjects( $select );
-
+        $tourItems = $tiTable->fetchObjects( $tiSelect );
+        
+        $glSelect = $glTable->getSelect();
+        $glSelect->where( 'item_id = ?', array( $tourItems[0]->item_id ) );
+        $geolocations = $glTable->fetchObjects( $glSelect );
+        $startLocation = $geolocations[0];
+        
         #map tour items to items
         $generator = function($tourItem){
             $result = array(
@@ -43,7 +50,8 @@ class Api_Tour extends Omeka_Record_Api_AbstractRecordAdapter
             'slug' => $record->slug,
             'postscript_text' => $record->postscript_text,
             'tour_image' => $record->tour_image,
-            'items' => $items
+            'items' => $items,
+            'start' => $startLocation
         );
         return $representation;
     }
